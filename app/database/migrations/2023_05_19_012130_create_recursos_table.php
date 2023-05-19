f445e55392e3df2f5fac347c0c85c700286ea35c
lucas oliveira <lucas.fullstack.dev@gmail.com>
2023-05-18 22:34:39 -0300

    feat: criando tabelas e populando os recursos existentes

---
+++ app/database/migrations/2023_05_19_012130_create_recursos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecursosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recursos', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+
+            $table->string('recurso')->unique();
+            $table->float('saldo_disponivel');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recursos');
+    }
+}
