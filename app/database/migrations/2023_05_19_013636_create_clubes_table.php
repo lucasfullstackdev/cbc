3efe5cdf5333b889f74243e74e591704d07d1e7a
lucas oliveira <lucas.fullstack.dev@gmail.com>
2023-05-18 22:40:24 -0300

    feat: criando tabela de clubes

---
+++ app/database/migrations/2023_05_19_013636_create_clubes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClubesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clubes', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+
+            $table->string('clube')->unique();
+            $table->float('saldo_disponivel');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clubes');
+    }
+}
